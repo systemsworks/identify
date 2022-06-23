8e974ade3a220440644e86b8a7adf6c84a744681
systemsworks <108077944+systemsworks@users.noreply.github.com>
2022-06-24 00:44:54 +1000

    Update identify-quiz-v1.js
    Added displayname code

--- identify-quiz-v1.js
+++ identify-quiz-v1.js
@@ -1,3 +1,25 @@
+var displayname;
+
+
+function identify_form_form_change() {
+      displayname = getValueFromInput("displayname");
+
+
+      $('#displayname').text(displayname);
+}
+
+  $("[data-name=identify-form] input").on("input", function () {
+    identify_form_form_change();
+  });
+
+  var clickedRadioButtonValue = $("input", this).val();
+
+$("[data-name=identify-form] label.w-radio").on("click", function () {
+  clickedRadioButtonValue = $("input", this).val();
+  identify_form_form_change();
+});
+
+
 var displayname, my_0_think, my_1_define, my_2_nature, my_3_stakeholders, my_4_solutions, my_5_toolbox;
 
 
